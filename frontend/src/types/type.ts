af98bb19dc7767e763fed61db16f0043d469849c
vijaysingh@8650 <rathorevijay995@gmail.com>
2024-08-27 20:34:58 +0530

    table started

---
+++ frontend/src/types/type.ts
@@ -0,0 +1,15 @@
+export type TypeResponseOfOrders = {
+    id: number;
+    orderId: string;
+    customerName: string;
+    status:OrderStatusEnum;
+    createdAt: Date;
+}
+
+enum OrderStatusEnum {
+    Pending,
+    Accepted,
+    Shipped,
+    Delivered,
+    Cancelled
+  }
